78209c032b1d9041f28078726406486b891c99d1
pauescalantec <pauescalante8@hotmail.com>
2017-11-06 08:11:29 -0600

    Analytics feed functionality not working with PHP yet, affixed to move with scroller

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
 var currentTopic = "";
 var currentUser = "";
+var currentWidthAnalytics = 0;
+
+$(window).on('resize', function() {
+    currentWidthAnalytics = $("#analyticsFeed").width();
+
+    if($(window).width() < 768){
+        $("#analyticsFeed").remove();
+    }
+
+    else {
+        if (!($("#analyticsFeed").length)) {
+            // Add analytics feed again
+            $("#analyticsGrid").html("");
+            $("#analyticsGrid").append('<div class="well" id="analyticsFeed" data-spy="affix" data-offset-top="65"> </div>');
+            loadAnalyticsField();
+        }
+    }
+
+});
 
 $(document).ready(function(){
     // Menu control
@@ -20,36 +39,80 @@ $(document).ready(function(){
     $('#homeSection').on('classChange', function() {
         // Load topics
         loadTopicCards();
+        loadAnalyticsField();
     });
+
+    $('#analyticsFeed').on('affix.bs.affix', function () {
+        var $affixElement = $('div[data-spy="affix"]');
+        if(currentWidthAnalytics == 0) {
+            currentWidthAnalytics = $("#analyticsFeed").width();
+        }
+        $affixElement.width(currentWidthAnalytics);
+   });
     
     // Load topics
     loadTopicCards();
+
+    // Load Analytics feed
+    loadAnalyticsField();
 
     // Load topics
     loadUserFullName();
 });
 
+function loadAnalyticsField(){
+    var visitedHTML = helperCreateAnalytics("visitedPanelHeading", "Most Visited Topic", getVisitedTopic());
+
+    var favoriteHTML = helperCreateAnalytics("favoritePanelHeading", "Your Favorite Topic", getFavoriteTopic()); 
+
+    var highestHTML = helperCreateAnalytics("highestPanelHeading", "Highest Ranked Tutor", getHighestTutor());   
+    
+    $("#analyticsFeed").html("");
+    $("#analyticsFeed").append(visitedHTML).hide().fadeIn(300);
+    $("#analyticsFeed").append(favoriteHTML).hide().fadeIn(300);
+    $("#analyticsFeed").append(highestHTML).hide().fadeIn(300);
+}
+
 function loadUserFullName(){
-    currentUser = "Paulina";
+    currentUser = getCurrentUser();
     // Add to profile section
     $("#navbarItemDropdown").html('<span id="navbarItemProfile" class="glyphicon glyphicon-user"></span> ' + currentUser);
 }
 
+function getFavoriteTopic(){
+    return "C++";
+}
+
+function getHighestTutor(){
+    return "Cristina Jimenez";
+}
+
+function getVisitedTopic(){
+    return "Python";
+}
+
+function getCurrentUser(){
+    return "Paulina";
+}
+
+function getTopicCards(){
+    return [{
+        topicName: "UX/UI",
+        topicDescription:"UX & UI have always been a power couple. UX design is an analytical and technical skill that co-relates to the UI graphic design to increase of effectiveness in the use of the applications developed.",
+        topicImage:"Media/cardImage1.jpg"
+    }];
+}
+
 function loadTopicCards(){
-    listTopics = [{
-                topicName: "UX/UI",
-                topicDescription:"UX & UI have always been a power couple. UX design is an analytical and technical skill that co-relates to the UI graphic design to increase of effectiveness in the use of the applications developed.",
-                topicImage:"Media/cardImage1.jpg"
-            }];
+    listTopics = getTopicCards();
 
     for (i = 1; i < 10; i++) { 
         // Instead of 0 iterate through index
         var topicCardHTML = helperCreateCardHTML(listTopics[0], i);
         var topicCounterId = "#topic" + i;
 
+        $(topicCounterId).html("");
         $(topicCounterId).append(topicCardHTML).hide().fadeIn(i*400);
-
-        
     }
 
     $("[name='askQuestionButton']").on("click", function() {
@@ -67,8 +130,7 @@ function helperCreateCardHTML(topicData, idNumber){
         return buildHTML;
 }
 
-function AutoGrowTextArea(textField)
-{
+function AutoGrowTextArea(textField){
   if (textField.clientHeight < textField.scrollHeight)
   {
     textField.style.height = textField.scrollHeight + "px";
@@ -78,4 +140,9 @@ function AutoGrowTextArea(textField)
         (textField.scrollHeight * 2 - textField.clientHeight) + "px";
     }
   }
+}
+
+function helperCreateAnalytics(idField, titleField, dataField){
+    var analyticsHTML = '<div id="' + idField + '" class="well"> <h4>' + titleField + '</h4><p>' + dataField + '</p> </div>';
+    return analyticsHTML;
 }
